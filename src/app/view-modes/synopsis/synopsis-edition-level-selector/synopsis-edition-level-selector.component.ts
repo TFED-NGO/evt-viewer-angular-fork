3fd4c953f9012adffcf12a1500fca4d1d0bc4d38
ghevi.sartor <ghevi.sartor@silentwave.eu>
2024-09-13 16:56:40 +0200

    Changed synopsis defaultEditionLevel
    It's passed as a parameter instead of taking the first one from the configuration

--- src/app/view-modes/synopsis/synopsis-edition-level-selector/synopsis-edition-level-selector.component.ts
+++ src/app/view-modes/synopsis/synopsis-edition-level-selector/synopsis-edition-level-selector.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { AppConfig, EditionLevel } from 'src/app/app.config';
 import { EvtIconInfo } from 'src/app/ui-components/icon/icon.component';
 
@@ -8,9 +8,9 @@ import { EvtIconInfo } from 'src/app/ui-components/icon/icon.component';
   styleUrls: ['./synopsis-edition-level-selector.component.scss']
 })
 export class SynopsisEditionLevelSelectorComponent {
-  private editionLevels = (AppConfig.evtSettings.edition.availableEditionLevels || []).filter((el) => el.enable);
-  selectableEditionLevels: EditionLevel[] = this.editionLevels.filter((el) => !el.hidden);
-  editionLevel = this.selectableEditionLevels[0];
+  private readonly editionLevels = (AppConfig.evtSettings.edition.availableEditionLevels || []).filter((el) => el.enable);
+  readonly selectableEditionLevels: EditionLevel[] = this.editionLevels.filter((el) => !el.hidden);
+  @Input() editionLevel: EditionLevel;
   icon: EvtIconInfo = {
     icon: 'layer-group', // TODO: Choose better icon
     additionalClasses: 'me-2',
