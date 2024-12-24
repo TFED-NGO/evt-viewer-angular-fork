1667f35c4c755c79d4fd48f421955c0a4dcd01e4
Ghevi <ghevi96@gmail.com>
2024-12-24 10:20:18 +0100

    Added options to takeWhile to include one additional item after condition ends loop

--- src/app/extensions/array.extensions.ts
+++ src/app/extensions/array.extensions.ts
@@ -1,7 +1,7 @@
 declare global {
     interface Array<T> {
         skipWhile(predicate: (value: T) => boolean): T[];
-        takeWhile(predicate: (value: T) => boolean): T[];
+        takeWhile(predicate: (value: T) => boolean, options?: { includeLastItem: boolean }): T[];
     }
 }
 
@@ -13,11 +13,12 @@ Array.prototype.skipWhile = function <T>(this: T[], predicate: (value: T) => boo
     return this.slice(index);
 };
 
-Array.prototype.takeWhile = function <T>(this: T[], predicate: (value: T) => boolean): T[] {
+Array.prototype.takeWhile = function <T>(this: T[], predicate: (value: T) => boolean, options?: { includeLastItem: boolean }): T[] {
     let index = 0;
     while (index < this.length && predicate(this[index])) {
         index++;
     }
+    if (index !== 0 && options?.includeLastItem) index++;
     return this.slice(0, index);
 };
 
